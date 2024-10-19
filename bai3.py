3501d92f69cce9792b727814d920aed8a224647f
YxngHuu <bamecon321@gmail.com>
2024-10-19 16:16:07 +0700

    Update bai3.py

--- bai3.py
+++ bai3.py
@@ -4,36 +4,62 @@ Created on Mon Sep 30 10:01:42 2024
 
 @author: ACER
 """
+#Bài tập cơ bản:
+#Bài 3:
+#1
 import random
-def tao_day_ngau_nhien(N):
-  seqA = []
-  for i in range(N):
-    kieu_du_lieu = random.choice(['int', 'float'])          
-    if kieu_du_lieu == 'int':   
-      so = random.randint(-79, 39)
-    else:
-      so = round(random.uniform(-79, 39), 2)
-      seqA.append(so)
-      return seqA
-N = random.randint(30, 80)
-seqA = tao_day_ngau_nhien(N)
-print("Danh sách ngẫu nhiên:", seqA)        
+def tao_seqA():
+    n = random.randint(30,80)
+    seqA = [n]
+    for i in range(n+1):
+        if random.randint(0,1) == 0:
+            seqA.append(round(random.uniform(-79,39),2))
+        else:
+            seqA.append(random.randint(-79,39))
+    return seqA 
+#2
+def ktra_dulieu(seqA):
+    return [type(i).__name__ for i in seqA]  
+#3
+def thongke(seqA):
+    return len(seqA)
 
-def kiem_tra_kieu_du_lieu(danh_sach):
-  for i, phan_tu in enumerate(danh_sach):
-    print(f"Phần tử thứ {i+1}: {phan_tu}, Kiểu dữ liệu: {type(phan_tu)}")
+#4
+def sapxep_seqB(seqA):
+    return sorted(seqA)
+
+#5
+def trungbinh_seqA(seqA):
+    return sum(seqA) / len(seqA)
+
+#6
+#[1234] = trung bình giữa 2 phần tử 
+
+def trungbinh_seqB(seqB):
+    n = len(seqB)
+    if n%2 == 0:
+        return (seqB[n//2 - 1] + seqB[n//2])/2
+    else:
+        return seqB[n//2]
     
-def thong_ke_so_luong(danh_sach):
-      return len(danh_sach) 
-  
-def sap_xep_tang_dan(danh_sach):
-      danh_sach_sap_xep = sorted(danh_sach)
-      return danh_sach_sap_xep
-  
-def tinh_trung_binh(danh_sach):
-  if len(danh_sach) == 0:
-    return None  
-  else:
-    tong = sum(danh_sach)
-    trung_binh = tong / len(danh_sach)
-    return trung_binh
+#7
+def khoangcach(seq):
+    return max(seq) - min(seq)
+
+#8
+def sosanh(seqA,seqB):
+    return trungbinh_seqA(seqA), trungbinh_seqB(seqB), trungbinh_seqA(seqA) == trungbinh_seqB(seqB)
+
+if __name__=="__main__":
+    seqA = tao_seqA()
+    print(seqA)
+    print(ktra_dulieu(seqA))
+    print(thongke(seqA))
+    seqB = sapxep_seqB(seqA)
+    print(seqB)
+    print(trungbinh_seqA(seqA))
+    print(trungbinh_seqB(seqB))
+    print(khoangcach(seqA))
+    print(khoangcach(seqB))
+    sosanh_AB=sosanh(seqA,seqB)
+    print(sosanh_AB)
